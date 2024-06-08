b881d7d40ed52dc9a7077b7e4633847abeb48e3b
ArTeria21 <Artyom08112006@gmail.com>
2024-06-08 20:30:56 +0300

    Created API token autorisation

--- VKode/Users/urls.py
+++ VKode/Users/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
+from django.urls import path
+from .views import CustomTokenObtainPairView, UserProfileView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 app_name = 'users'
 
 urlpatterns = [
+	path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/profile/', UserProfileView.as_view(), name='user_profile'),
     path('login/', views.LoginUser.as_view(), name='login'),
     path('registration/', views.RegisterUser.as_view(), name='registration'),
     path('logout/', views.logout_user, name='logout')
--- VKode/Users/views.py
+++ VKode/Users/views.py
@@ -5,17 +5,34 @@ from django.contrib.auth import logout
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from . import forms
+from .forms import UserCreationForm, UserLogin, UserRegistration
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = UserSerializer
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
 
 
 class LoginUser(LoginView):
-    form_class = forms.UserLogin
+    form_class = UserLogin
     template_name = 'users/login.html'
     title = 'Авторизация'
 
 
 class RegisterUser(CreateView):
-    form_class = forms.UserRegistration
+    form_class = UserRegistration
     template_name = 'users/register.html'
     success_url = reverse_lazy('index')
     title = 'Регистрация'
