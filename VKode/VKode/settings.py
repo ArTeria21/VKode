a93316611cc18da3a1fb91861bb0fa70665425c2
ArTeria21 <Artyom08112006@gmail.com>
2024-06-17 10:15:06 +0300

    prepared project for production

--- VKode/VKode/settings.py
+++ VKode/VKode/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3jc3s#-18okbq=_idkb*r&d*$@5+*(aewl+wf+_%qql$-39b#q"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
